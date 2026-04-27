997a7e5cc984cb7dde545589fc01f6797d13b5a5
Jarl <okbexx@users.noreply.github.com>
2026-04-27 22:02:01 +0800

    fix: inline astro styles for pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://okbexx.github.io',
-  base: '',
-  markdown: {
-    shikiConfig: {
-      theme: 'github-dark',
-    },
+  base: '/',
+  build: {
+    inlineStylesheets: 'always',
   },
 });
